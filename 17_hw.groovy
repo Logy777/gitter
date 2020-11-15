7571aa529ce598f0a15b4c894797395412906920
Dzianis Zavoiskikh <6557587@gmail.com>
2020-11-15 10:35:46 +0300

    speedtest accept license

--- 17_hw.groovy
+++ 17_hw.groovy
@@ -49,7 +49,7 @@ pipeline {
             steps {
                 echo 'Testing..'
                 sh """
-                speedtest
+                speedtest --accept-license
                 """
             }
         }
